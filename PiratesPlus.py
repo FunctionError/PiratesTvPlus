36edf765ffdd744942af1ada6287e25422feacb7
Function Error <138248655+FunctionError@users.noreply.github.com>
2024-05-08 13:58:42 +0600

    First Commit 😍

---
+++ PiratesPlus.py
@@ -0,0 +1,83 @@
+import os
+import requests
+
+def read_channels_txt(channels_file):
+    with open(channels_file, 'r', encoding='utf-8') as file:
+        return [line.strip() for line in file.readlines()]
+
+def extract_logo(line, default_logo):
+    try:
+        logo = line.split('tvg-logo="')[1].split('"')[0]
+        if not logo:
+            return default_logo
+        else:
+            return logo
+    except IndexError:
+        return default_logo
+
+def filter_channels(playlist_sources, channel_categories):
+    default_logo = "https://raw.githubusercontent.com/FunctionError/Logos/main/Pirates-Tv.png"
+    default_source = 'https://raw.githubusercontent.com/FunctionError/Logos/main/PiratesTv/master.m3u8'
+    filtered_channels = {}
+    for source in playlist_sources:
+        response = requests.get(source)
+        lines = response.text.split('\n')
+        for i in range(len(lines)):
+            if lines[i].startswith('#EXTINF') and len(lines[i].split(',')) > 1:
+                name = lines[i].split(',')[1].strip()
+                url = lines[i + 1].strip()
+                category = channel_categories.get(name)
+                if category:
+                    logo = extract_logo(lines[i], default_logo)
+                    if check_channel_status(url):
+                        if name not in filtered_channels:
+                            filtered_channels[name] = {'url': url, 'logo': logo, 'category': category, 'source': source}
+                    else:
+                        # If channel source is inactive, use default source
+                        filtered_channels[name] = {'url': default_source, 'logo': logo, 'category': category, 'source': source}
+    return filtered_channels
+
+def check_channel_status(url):
+    try:
+        response = requests.head(url, timeout=5)
+        return response.status_code == 200
+    except requests.RequestException:
+        return False
+
+def write_m3u(output_file, channels):
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write("#EXTM3U\n")
+        file.write("# All the links in this file are collected from public sources. If anyone wants to remove their source, please let us know. We respect your opinions and efforts, so we will not object to removing your source. https://www.t.me/PiratesTv_ch\n")
+        for name, data in channels.items():
+            url = data['url']
+            logo = data['logo']
+            category = data['category']
+            source = data['source']
+            file.write(f"#EXTINF:-1 tvg-logo=\"{logo}\" tvg-category=\"{category}\",{name}\n")
+            file.write(f"{url}\n")
+
+def main():
+    playlist_sources = [
+        os.getenv('PLAYLIST_SOURCE_URL_1'),
+        os.getenv('PLAYLIST_SOURCE_URL_2'),
+        os.getenv('PLAYLIST_SOURCE_URL_3')
+    ]
+    channels_file = 'channels.txt'
+    output_file = 'PiratesPlus.m3u'
+
+    # Read channel names and categories from channels.txt
+    channel_categories = {}
+    with open(channels_file, 'r', encoding='utf-8') as file:
+        for line in file:
+            parts = line.strip().split(',')
+            if len(parts) == 2:
+                channel_categories[parts[0]] = parts[1]
+
+    # Filter channels based on channel categories and status
+    filtered_channels = filter_channels(playlist_sources, channel_categories)
+
+    # Write filtered channels to output M3U file
+    write_m3u(output_file, filtered_channels)
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,81 @@
+
+# Biggest Pirates IPTV on GitHub
+
+This script is designed to merge and keep up-to-date m3u links collected from public sources.
+
+
+<p align="center">
+
+  <a href="https://www.python.org/">
+    <img src="https://img.shields.io/badge/Made_With-Python_3.12%2B-blue"
+         alt="Python Version">
+  <a href="https://gitter.im/amitmerchant1990/electron-markdownify">
+    <img src="https://img.shields.io/badge/Made%20in-Bangladesh_🇧🇩-green?colorA=%23ff0000&colorB=%23017e40&style=flat-square"
+         alt="Made in Bangladesh">
+  </a>
+  <a href="https://github.com/FunctionError/PiratesTv/actions/workflows/main.yml">
+    <img src="https://github.com/FunctionError/PiratesTvPlus/actions/workflows/main.yml/badge.svg"
+         alt="Build Status">
+  </a>
+<a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FFunctionError%2FPiratesTvPlus&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a>
+
+![Logo](https://i.ibb.co/zNsRhgv/Pirates-Tv-plus.png)
+
+
+## Authors
+
+- [@FunctionError](https://www.github.com/FunctionError)
+
+### 📺 Join us 👇
+
+[![telegram](https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white)](https://t.me/piratestv_ch)
+[![discord](https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=white)](https://discord.gg/piratestv403)
+
+
+## Screenshots
+
+![App Screenshot](https://i.ibb.co/ssVqx8c/ssofvlc.png)
+
+
+## Playlist Links 👇
+
+Primarily focused on Bangladeshi users (Use Vlc for PC)
+
+```bash
+https://raw.githubusercontent.com/FunctionError/PiratesTvPlus/main/piratesplus.m3u
+```
+
+
+# Contributing
+
+Contributions are always welcome! Feel Free To Clone This Project. For Major Changes, Please Open An Issue First To Discuss What You Would Like To Change Or Add, Thank You ! 🖤
+
+
+
+# Credits
+ <details close>
+<summary> See hare 😍
+
+</summary>
+
+This repository collects m3u files collected from multiple public sources. There is no specific source here. If you think the m3u source used in this repository is yours, please open an issue and let us know, we will remove your source. We will always try to give you full credit, because I believe that everyone has the right to showcase their talent to this beautiful world.
+
+   </details>
+
+# Play List sources
+ <details close>
+<summary> See hare 😍
+
+</summary>
+
+
+- [@FunctionError](https://www.github.com/FunctionError)
+
+- [@subir](https://github.com/subirkumarpaul/)
+
+- ### 
+
+   </details>
+
+# **License**
+[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://github.com/FunctionError/PiratesTv/blob/main/LICENSE)
